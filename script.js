0d99e5759278c34ac8ee5097f80ee11827c6aca8
Bohdan Kladkovyi <b.kladkovyi@inchkiev.ua>
2019-10-27 11:58:10 +0200

    tabs

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
+// Show the first tab and hide the rest
+$(document).ready(function(){
+$('#tabs-nav li:first-child').addClass('active');
+$('.service-block-content-text').hide();
+$('.service-block-content-text:first').show();
 
-
-    $( function() {
-        $( "#tabs" ).tabs();
-    } );
+// Click function
+$('#tabs-nav li').click(function(){
+  $('#tabs-nav li').removeClass('active');
+  $(this).addClass('active');
+  $('.service-block-content-text').hide();
+  
+  let activeTab = $(this).find('a').attr('href');
+  $(activeTab).fadeIn();
+  return false;
+});
+})
